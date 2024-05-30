9164875cf29a7607979dacc2c23515d410f81df9
El Majjani Youssef <youssef.elmajjani@gmail.com>
2024-05-30 17:41:32 +0200

    Update RenewableList.java

--- src/main/java/RenewableList.java
+++ src/main/java/RenewableList.java
@@ -1,12 +1,13 @@
-package org.example;
-
 import java.util.LinkedList;
 
-public class RenewableList<E> extends LinkedList<E> {
+public class RenewableList<E> {
     private int capacity;
+    private final LinkedList<E> list;
+
 
     public RenewableList(int capacity) {
         this.capacity = capacity;
+        list = new LinkedList<>();
     }
 
     public int getCapacity() {
@@ -14,20 +15,45 @@ public class RenewableList<E> extends LinkedList<E> {
     }
 
     public void setCapacity(int newCapacity) {
-        if (capacity > newCapacity)
-            removeRange(newCapacity,size());
+        if (newCapacity < size()) {
+            int size = size();
+            for (int i =0 ; i < size - newCapacity; i++)
+                list.removeLast();
+        }
 
         this.capacity = newCapacity;
     }
 
-    @Override
-    public boolean add(E o) {
-        if (capacity <= size()){
-            removeLast();
+    public void add(E o) {
+        if (capacity <= list.size()){
+            list.removeLast();
         }
 
-        addFirst(o);
-        return true;
+        list.addFirst(o);
     }
 
+    public int size(){
+        return list.size();
+    }
+
+    public void remove() {
+        list.removeLast();
+    }
+
+    public void remove(int index) {
+        list.remove(index);
+    }
+
+    public E get (int index) {
+        return list.get(index);
+    }
+
+    public E get() {
+        return list.getFirst();
+    }
+
+    @Override
+    public String toString() {
+        return list.toString();
+    }
 }
